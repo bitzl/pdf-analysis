95de0085641f2dc19e755ad0391452e49418c7c9
marcus.bitzl@bsb-muenchen,de <marcus.bitzl@bsb-muenchen,de>
2014-09-04 14:11:00 +0200

    Added dummy data for starters.

---
+++ src/java/pdfanalysis/Main.java
@@ -0,0 +1,14 @@
+package pdfanalysis;
+
+
+public class Main {
+
+    public static String what() {
+        return "great";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Something " + what() + " will be here.");
+    }
+
+}
---
+++ src/test/pdfanalysis/MainTest.java
@@ -0,0 +1,14 @@
+package pdfanalysis;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testWhat() {
+        Assert.assertSame("great", Main.what());
+    }
+
+}
