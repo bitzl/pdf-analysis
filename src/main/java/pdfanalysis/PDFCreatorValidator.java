15cb462c402f6e6484e2ae7797ab9096f5838a6c
marcus.bitzl@bsb-muenchen,de <marcus.bitzl@bsb-muenchen,de>
2014-09-05 15:36:45 +0200

    Added exception handler.

--- src/main/java/pdfanalysis/PDFCreatorValidator.java
+++ src/main/java/pdfanalysis/PDFCreatorValidator.java
@@ -1,6 +1,8 @@
 package pdfanalysis;
 
 import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pdfanalysis.export.CSV;
 import pdfanalysis.export.InfoWriter;
 import pdfanalysis.model.PdfInfo;
@@ -10,12 +12,15 @@ import pdfanalysis.validator.PdfValidation;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
 public class PDFCreatorValidator {
 
-    public static void main(String[] args) throws IOException {
+    final Logger logger = LoggerFactory.getLogger(PDFCreatorValidator.class);
+
+    public void main() throws IOException {
         String csvFilename = "data.csv";
 
         String sourcePath = PdfUtilities.chooseFolder();
@@ -34,8 +39,14 @@ public class PDFCreatorValidator {
         while (pdfFiles.hasNext()) {
             File pdfFile = pdfFiles.next();
             if (!pdfFile.isDirectory()) {
-                PdfInfo info = metadataReader.read(pdfFile);
-                List<PdfInfo> results = pdfValidation.validate(pdfFile, info);
+                List<PdfInfo> results = Collections.emptyList();
+                try {
+                    PdfInfo info = metadataReader.read(pdfFile);
+                    results = pdfValidation.validate(pdfFile, info);
+                }
+                catch (Exception e) {
+                    logger.error("Error analyzing " + pdfFile.getAbsolutePath(), e);
+                }
                 for (PdfInfo result : results) {
                     csv.write(result);
                 }
@@ -43,6 +54,11 @@ public class PDFCreatorValidator {
         }
 
         writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        PDFCreatorValidator creatorValidator = new PDFCreatorValidator();
+        creatorValidator.main();
     }
 
 }
